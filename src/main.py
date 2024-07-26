210f878337dfc7dc21725e81bc4e8e146ba2e5c2
attributeofextension <leah.carr00@gmail.com>
2024-07-26 15:55:51 +1000

    text_node_to_html_node function in main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,17 @@
 from textnode import TextNode
+from htmlnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case "text": return LeafNode(text_node.text)
+        case "bold": return LeafNode(text_node.text, "b")
+        case "italic": return LeafNode(text_node.text, "i")
+        case "code": return LeafNode(text_node.text, "code")
+        case "link": return LeafNode(text_node.text, "a", {"href": text_node.url})
+        case "image": return LeafNode("", "img", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("Text node type is not acceptable")
+
 def main():
     print("hello world")
     textnode = TextNode("This is a text node", "bold", "https://www.boot.dev")
---
+++ src/test_main.py
@@ -0,0 +1,75 @@
+import unittest
+
+from main import text_node_to_html_node
+from textnode import TextNode
+from htmlnode import LeafNode
+
+
+class TestTextNodeToHTML(unittest.TestCase):
+    def setUp(self):
+        self.text_type_text_node = TextNode("raw text", "text","not shown")
+        self.text_type_bold_node = TextNode("bold text", "bold", "not shown")
+        self.text_type_italic_node = TextNode("italic text", "italic", "not shown")
+        self.text_type_code_node = TextNode("code text", "code", "not shown")
+        self.text_type_link_node = TextNode("link text", "link", "shown in href prop")
+        self.text_type_image_node = TextNode("shown in alt prop", "image", "shown in src prop")
+        self.text_type_script_node = TextNode("raises exception", "script", "not shown")
+
+    def test_text_type_text(self):
+        node = text_node_to_html_node(self.text_type_text_node)
+        self.assertIsInstance(node, LeafNode)
+        self.assertEqual(node.to_html(), "raw text")
+        self.assertEqual(node.to_html(), self.text_type_text_node.text)
+
+    def test_text_type_bold(self):
+        node = text_node_to_html_node(self.text_type_bold_node)
+        self.assertIsInstance(node, LeafNode)
+        self.assertEqual(node.to_html(), "<b>bold text</b>")
+        self.assertIn(self.text_type_bold_node.text, node.to_html())
+        self.assertEqual(node.to_html()[0:3], "<b>")
+        self.assertEqual(node.to_html()[-4:], "</b>")
+
+    def test_text_type_italic(self):
+        node = text_node_to_html_node(self.text_type_italic_node)
+        self.assertIsInstance(node, LeafNode)
+        self.assertEqual(node.to_html(), "<i>italic text</i>")
+        self.assertIn(self.text_type_italic_node.text, node.to_html())
+        self.assertEqual(node.to_html()[0:3], "<i>")
+        self.assertEqual(node.to_html()[-4:], "</i>")
+
+    def test_text_type_code(self):
+        node = text_node_to_html_node(self.text_type_code_node)
+        self.assertIsInstance(node, LeafNode)
+        self.assertEqual(node.to_html(), "<code>code text</code>")
+        self.assertIn(self.text_type_code_node.text, node.to_html())
+        self.assertEqual(node.to_html()[0:6], "<code>")
+        self.assertEqual(node.to_html()[-7:], "</code>")
+
+    def test_text_type_link(self):
+        node = text_node_to_html_node(self.text_type_link_node)
+        self.assertIsInstance(node, LeafNode)
+        self.assertEqual(node.to_html(), "<a href=\"shown in href prop\">link text</a>")
+        self.assertIn(self.text_type_link_node.text, node.to_html())
+        self.assertIn("href", node.to_html())
+        self.assertIn(self.text_type_link_node.url, node.to_html())
+        self.assertEqual(node.to_html()[0:2],"<a")
+        self.assertEqual(node.to_html()[-4:], "</a>")
+
+    def test_text_type_image(self):
+        node = text_node_to_html_node(self.text_type_image_node)
+        self.assertIsInstance(node, LeafNode)
+        self.assertEqual(node.to_html(), "<img src=\"shown in src prop\" alt=\"shown in alt prop\"></img>")
+        self.assertIn(self.text_type_image_node.text, node.to_html())
+        self.assertIn("src", node.to_html())
+        self.assertIn(self.text_type_image_node.url, node.to_html())
+        self.assertIn("alt", node.to_html())
+        self.assertEqual(node.to_html()[0:4],"<img")
+        self.assertEqual(node.to_html()[-6:], "</img>")
+
+    def test_text_type_script(self):
+        with self.assertRaisesRegex(Exception, "Text node type is not acceptable"):
+            node = text_node_to_html_node(self.text_type_script_node)
+
+
+if __name__ == '__main__':
+    unittest.main()
