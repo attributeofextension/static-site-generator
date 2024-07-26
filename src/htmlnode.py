e26389458291b48cf008b8a289ff8580ad8ea3ef
attributeofextension <leah.carr00@gmail.com>
2024-07-26 14:57:49 +1000

    create and test ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,13 +20,32 @@ class HTMLNode():
                 tag_text += f" {key}=\"{value}\""
             return tag_text
 
+class ParentNode(HTMLNode):
+    def __init__(self, children, tag=None, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag cannot be None")
+        if self.children is None:
+            raise ValueError("Children cannot be None")
+        if len(self.children) == 0:
+            raise ValueError("Children cannot be empty")
+
+        inner_markup = ""
+        for child in self.children:
+            inner_markup += child.to_html()
+
+        return f"<{self.tag}{self.props_to_html()}>{inner_markup}</{self.tag}>"
+
+
 class LeafNode(HTMLNode):
     def __init__(self, value, tag=None, props=None):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value is None:
-            raise ValueError
+            raise ValueError("Value cannot be None")
         if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,65 @@
+import unittest
+
+from htmlnode import HTMLNode, ParentNode, LeafNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node1 = HTMLNode(None, None, None, {"href": "https://www.google.com", "target": "_blank"})
+        self.assertEqual(node1.props_to_html()," href=\"https://www.google.com\" target=\"_blank\"")
+
+    def test_to_html(self):
+        node1 = HTMLNode(None, None, None, {"href": "https://www.google.com", "target": "_blank"})
+        with self.assertRaises(NotImplementedError):
+            node1.to_html()
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node1 = LeafNode("This is a paragraph of text.", "p")
+        self.assertEqual(node1.to_html(), "<p>This is a paragraph of text.</p>")
+
+    def test_to_html_no_tag(self):
+        node1 = LeafNode("This is a paragraph of text.")
+        self.assertEqual(node1.to_html(),"This is a paragraph of text.")
+
+    def test_to_html_no_value(self):
+        node1 = LeafNode(None)
+        with self.assertRaisesRegex(ValueError, "Value cannot be None"):
+            node1.to_html()
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_children_is_none(self):
+        node1 = ParentNode(None, "ul")
+        with self.assertRaisesRegex(ValueError, "Children cannot be None"):
+            node1.to_html()
+
+    def test_to_html_children_is_empty(self):
+        node1 = ParentNode([], "ul")
+        with self.assertRaisesRegex(ValueError, "Children cannot be empty"):
+            node1.to_html()
+
+    def test_to_html_tag_is_none(self):
+        node1 = ParentNode([LeafNode("This is a paragraph of text.")], None)
+        with self.assertRaisesRegex(ValueError, "Tag cannot be None"):
+            node1.to_html()
+
+class TestDOMTree(unittest.TestCase):
+    def setUp(self):
+        self.p1 = LeafNode("This is paragraph 1", "p", {"class": "p1"})
+        self.p2 = LeafNode("This is paragraph 2", "p", {"class": "p2"})
+        self.div_with_two_children = ParentNode([self.p1,self.p2], "div", {"class": "parent"})
+        self.div_with_two_children_to_html_expected_output = "<div class=\"parent\"><p class=\"p1\">This is paragraph 1</p><p class=\"p2\">This is paragraph 2</p></div>"
+
+        self.p3 = LeafNode("This is paragraph 3", "p", {"class": "p3"})
+        self.div_with_one_parent_node_and_one_leaf_node = ParentNode([self.div_with_two_children,self.p3], "div", {"class": "grandparent"})
+        self.div_with_one_parent_node_and_one_leaf_node_to_html_expected_output = "<div class=\"grandparent\"><div class=\"parent\"><p class=\"p1\">This is paragraph 1</p><p class=\"p2\">This is paragraph 2</p></div><p class=\"p3\">This is paragraph 3</p></div>"
+
+    def test_div_with_two_children(self):
+        self.assertEqual(self.div_with_two_children.to_html(), self.div_with_two_children_to_html_expected_output)
+        self.assertEqual(self.div_with_two_children.to_html(), f"<div class=\"parent\">{self.p1.to_html()}{self.p2.to_html()}</div>")
+
+    def test_div_with_one_parent_node_and_one_leaf_node(self):
+        self.assertEqual(self.div_with_one_parent_node_and_one_leaf_node.to_html(), self.div_with_one_parent_node_and_one_leaf_node_to_html_expected_output)
+        self.assertEqual(self.div_with_one_parent_node_and_one_leaf_node.to_html(), f"<div class=\"grandparent\">{self.div_with_two_children.to_html()}{self.p3.to_html()}</div>")
+
+if __name__ == "__main__":
+    unittest.main()
