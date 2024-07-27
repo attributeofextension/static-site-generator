964b187540c84f93746dc441a57becc3330c7b53
attributeofextension <leah.carr00@gmail.com>
2024-07-27 20:20:57 +1000

    add some file handling to deploy static assets to public

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from textnode import TextNode
 from htmlnode import LeafNode
+from shutil import rmtree, copy
+from os import path, listdir, mkdir
 
+
+# deprecated
 def text_node_to_html_node(text_node):
     match text_node.text_type:
         case "text": return LeafNode(text_node.text)
@@ -12,9 +16,37 @@ def text_node_to_html_node(text_node):
         case _:
             raise Exception("Text node type is not acceptable")
 
+def copy_tree(src, dst):
+    contents = listdir(src)
+    for content in contents:
+        if path.isfile(path.join(src, content)):
+            print(f"Copying {path.join(src, content)} to {path.join(dst, content)}")
+            copy(path.join(src, content), path.join(dst, content))
+        elif path.exists(path.join(src, content)):
+            mkdir(path.join(dst, content))
+            copy_tree(path.join(src, content), path.join(dst, content))
+
+
+
+def deploy_static_assets_to_public():
+    # clean public/
+    if path.exists("public"):
+        rmtree("public")
+        mkdir("public")
+
+    if not path.exists("static"):
+        raise Exception("Static assets directory does not exist")
+    copy_tree("static", "public")
+
+
+
+
+
+
+
+
 def main():
-    print("hello world")
-    textnode = TextNode("This is a text node", "bold", "https://www.boot.dev", "This is a text node")
-    print(textnode)
+    deploy_static_assets_to_public()
+
 
 main()
